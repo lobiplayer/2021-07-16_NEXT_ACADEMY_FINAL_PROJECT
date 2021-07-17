fc04bf582a25544f13c38494b75246df13244705
Lobiplayer <jrobinmohan@gmail.com>
2021-07-17 20:11:10 +0800

    added requirements.txt, added Calendar.js. Installed react-native-calendar-strip and imported this. it is throwing an error

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Calendar from './pages/Calendar';
 
 export default function App() {
   return (
