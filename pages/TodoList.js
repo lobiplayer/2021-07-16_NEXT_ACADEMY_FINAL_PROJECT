46dd689d208b3cde9797930ecded09c61378812e
arizdanial <ariz.danial23@gmail.com>
2021-07-18 17:33:59 +0800

    purpose of push is to pull updated origin main. Please do not merge my App.js with remote"

--- pages/TodoList.js
+++ pages/TodoList.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
-export default function TodoList() {
-    return (
-        <View style={styles.container}>
-            <Text>Alette</Text>
-            <StatusBar style="auto" />
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+        <View style={styles.tasksWrapper}>
+            <Text style={styles.title}>Todo List</Text>
+            <View style={styles.items}>
+                {/* List of tasks created */}
+                <Task text={'Task one '}/> 
+                <Task text={'Task two '}/>
+            </View>
         </View>
-    );
+    </View>
+);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+},
+
+tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+},
+title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+},
+items: {
+  marginTop: 30, 
+},
 });
