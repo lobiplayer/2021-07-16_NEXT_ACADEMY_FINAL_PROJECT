c09397f3c2f8e9092ce487a8eb26002cef7dbabf
AletteXin <alettengxinling@gmail.com>
2021-07-24 12:54:31 +0800

    timer

---
+++ components/TimerRewards.js
@@ -0,0 +1,36 @@
+
+import React, { useEffect } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+
+const PomodoroRewards = () => {
+
+useEffect(() => {
+    fetch("http://192.168.1.120:5000/add_rewardslist", {
+        method: 'POST',
+        body: JSON.stringify({
+            points: '100',
+            user_id: '2',
+            task_completed: 'Pomodoro Timer on' + Date(),
+        }),
+
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        console.log(data);
+    }).catch(function (error) {
+        console.warn('Something went wrong.', error);
+    })
+}, []);
+
+
+return ({
+
+})
+
+}
+
+export default PomodoroRewards;
