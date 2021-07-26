f77d6310c9fc09394090f42981aaa2d926f0059b
Lobiplayer <jrobinmohan@gmail.com>
2021-07-26 20:20:45 +0800

    Calendar works now. Only post has a problem

--- OLD/Adddeadline copy.js
+++ OLD/Adddeadline copy.js
@@ -1,16 +1,59 @@
 import React, { useState } from "react";
-import { Text, View, TextInput, Button, Alert, Form } from "react-native";
+import { Text, View, TextInput, Button, Alert, Form, TouchableOpacity } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 import { Formik } from 'formik';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
+import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    StyledButton,
+    ButtonText,
+    Colors,
+    MsgBox,
+    Line,
+    ExtraView,
+    ExtraText,
+    TextLink,
+    TextLinkContent
+} from '../components/styles';
 
+const { brand, darkLight, primary } = Colors;
 
 const Adddeadline = () => {
     // const { control, handleSubmit, formState: { errors } } = useForm();
     // const onSubmit = data => console.log(data);
 
-    const [description, setdescription] = useState('');
+    const [deadlineText, setDeadlinetext] = useState('');
     const [subject, setSubject] = useState('');
-    const [date, setdate] = useState('');
+    const [datetime, setDatetime] = useState('');
+    const [show, setShow] = useState(false);
+    // default date of birth
+    const [date, setDate] = useState(new Date(2000, 0, 1));
+
+    // user date of birth selected
+    const [dob, setDob] = useState();
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(false);
+        setDate(currentDate);
+        setDob(currentDate);
+    }
+
+    const showDatePicker = () => {
+        setShow(true);
+    }
 
     // const sendData = (values) => {
     //     const response = await fetch('/add_deadline', {
@@ -27,8 +70,11 @@ const Adddeadline = () => {
     // }
 
     return (
+
+        
+
         <Formik
-            initialValues={{ description: '', subject: '', date: '' }}
+            initialValues={{ deadlineText: '', subject: '', datetime: '' }}
             onSubmit={values => sendData(values)}
         >
             {({ handleChange, handleBlur, handleSubmit, values }) => (
@@ -36,10 +82,10 @@ const Adddeadline = () => {
                     <InputBox
                         label = "Text"
                         placeholder="Chapter 1 paragraph"
-                        onChangeText={handleChange('description')}
-                        onBlur={handleBlur('description')}
-                        value={values.description}
-                        onChange={e => setdescription(e.target.value)}
+                        onChangeText={handleChange('deadlineText')}
+                        onBlur={handleBlur('deadlineText')}
+                        value={values.deadlineText}
+                        onChange={e => setDeadlinetext(e.target.value)}
                     />
                     <InputBox
                         label = "Subject"
@@ -49,17 +95,30 @@ const Adddeadline = () => {
                         value={values.subject}
                         onChange={e => setSubject(e.target.value)}
                     />
+
+                    <InputBox
+                        label="Date of Birth :"
+                        icon="calendar"
+                        placeholder="YYYY - MM - DD"
+                        placeholderTextColor={darkLight}
+                        onChange={handleChange('dateOfBirth')}
+                        onBlur={handleBlur('dateOfBirth')}
+                        value={dob ? dob.toDateString() : ''}
+                        isDate={true}
+                        editable={false}
+                        showDatePicker={showDatePicker}
+                    />
                     <InputBox
                         label="Due date and time"
-                        placeholder = "2021-07-27"
-                        onChangeText={handleChange('date')}
-                        onBlur={handleBlur('date')}
-                        value={values.date}
-                        onChange={e => setdate(e.target.value)}
+                        placeholder = "21-07-2021 12:00"
+                        onChangeText={handleChange('datetime')}
+                        onBlur={handleBlur('datetime')}
+                        value={values.datetime}
+                        onChange={e => setDatetime(e.target.value)}
                     />
                     <Button title="Submit" color='#0000c8' onPress = { async () => {
                         console.log(values)
-                        const response = await fetch("http://192.168.0.152:5000/add_deadline", {
+                        const response = await fetch('//192.168.0.152:5000/add_deadline', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
---
+++ OLD/Calendar.js
@@ -0,0 +1,124 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import CalendarStrip from 'react-native-calendar-strip';
+import Adddeadline from '../components/Adddeadline';
+import ModalAdddeadline from '../components/ModalAdddeadline';
+import { Agenda } from 'react-native-calendars';
+import { Card, Avatar } from 'react-native-paper';
+import moment from 'moment';
+
+
+const timeToString = (time) => {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+}
+
+const thisDay = moment().format("YYYY-MM-DD");
+
+
+export default function Calendar() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("http://192.168.0.152:5000/deadlines").then(response => response.json().then(data => {
+            console.log(data.deadlines)
+            setItems(data.deadlines);
+        })
+        );
+    }, []);
+    
+    const itemsCopy = 
+
+    const loadItems = (day) => {
+        setTimeout(() => {
+            for (let i = 0; i < 85; i++) {
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                const strTime = timeToString(time);
+                if (!items[strTime]) {
+                    items[strTime] = [];
+                    const numItems = Math.floor(Math.random() * 3 + 1);
+                    for (let j = 0; j < numItems; j++) {
+                        items[strTime].push({
+                            name: 'Mathematics',
+                            height: Math.max(50, Math.floor(Math.random() * 150))
+                        });
+                    }
+                }
+            }
+            const newItems = {};
+            Object.keys(items).forEach(key => {
+                newItems[key] = items[key];
+            });
+            setItems(newItems)
+        }, 1000);
+    };
+
+    const renderItem = (item) => {
+        return (
+            <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
+                <Card style={{ backgroundColor: '#fdb913' }}>
+                    <Card.Content>
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                            }}>
+                            <Text>{item.name}</Text>
+                            <Avatar.Icon size={40} icon="math-compass" color='#fdb913' backgroundColor='#0000c8' />
+                        </View>
+                    </Card.Content>
+                </Card>
+            </TouchableOpacity>
+        );
+    };
+
+    return (
+        <View style={{ flex: 1 }}>
+            <Agenda
+                items={items}
+                loadItemsForMonth={loadItems}
+                selected={thisDay}
+                renderItem={renderItem}
+                theme={{
+                    agendaTodayColor: '#0000c8',
+                    agendaDayTextColor: 'gray',
+                    agendaDayNumColor: 'gray',
+                    backgroundColor: '#ffffff',
+                    calendarBackground: '#ffffff',
+                    textSectionTitleColor: '#0000c8',
+                    textSectionTitleDisabledColor: '#d9e1e8',
+                    selectedDayBackgroundColor: '#fdb913',
+                    selectedDayTextColor: '#ffffff',
+                    todayTextColor: '#0000c8',
+                    dayTextColor: '#0000c8',
+                    textDisabledColor: '#0000c8',
+                    dotColor: '#fdb913',
+                    selectedDotColor: '#ffffff',
+                    arrowColor: 'orange',
+                    disabledArrowColor: '#d9e1e8',
+                    monthTextColor: '#0000c8',
+                    indicatorColor: '#0000c8',
+                    textDayFontFamily: 'monospace',
+                    textMonthFontFamily: 'monospace',
+                    textDayHeaderFontFamily: 'monospace',
+                    textDayFontWeight: '300',
+                    textMonthFontWeight: 'bold',
+                    textDayHeaderFontWeight: '300',
+                    textDayFontSize: 16,
+                    textMonthFontSize: 16,
+                    textDayHeaderFontSize: 16,
+                }}
+
+            />
+            <ModalAdddeadline />
+
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1 }
+});
