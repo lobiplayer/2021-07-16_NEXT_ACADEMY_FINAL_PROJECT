b5bdefac5f89b87ffe8299fd57b846ea210cf07b
AletteXin <alettengxinling@gmail.com>
2021-07-26 17:03:32 +0800

    Login functionality

--- pages/Homepage.js
+++ pages/Homepage.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useContext, useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, Button, Form } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import Homepagekeytask from '../components/Homepagekeytask';
 import Homepagekeydates from '../components/Homepagekeydates';
 import Homepagerewards from '../components/Homepagerewards';
@@ -12,20 +12,19 @@ const Homepage = () => {
 
     const [token, setToken] = useContext(LoginContext)
 
-    function handleSubmit(e){
+    const logout = (e) => {
         e.preventDefault();
         setToken("")
         console.log(token)
-        console.log("token changed")
+        console.log("token")
     }
 
 
     return (
         <ScrollView contentContainerStyle={styles.layout}>
             <Homepageheader />
-            <Form onSubmit={handleSubmit}>
-            <Button type="submit">Logout</Button>
-            </Form>
+            <Button title="Log Out" onPress={(e) => logout(e)}/>
+            <Text>The current user is {token}</Text>
             <Homepagekeytask />
             <Homepagekeydates />
             <Homepagerewards />
