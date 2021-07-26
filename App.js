3b752f68ce85bed42a222bf7cfdf54ac72dd2d22
AletteXin <alettengxinling@gmail.com>
2021-07-26 15:22:21 +0800

    Login link to backend

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from './components/Navigation'
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Welcome from './pages/Welcome';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginProvider, LoginContext } from './LoginContext';
+
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [token, setToken] = useContext(LoginContext)
+
+  console.log('token')
+  console.log(token)
+
+
   return (
-    // <NavigationContainer >
-    //   <Navigation />
-    // </NavigationContainer>
-    <Signup />
+    <LoginProvider>
+      <NavigationContainer >
+
+        {token == "" ?
+          <Stack.Navigator headerMode='none'>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="SignUp" component={Signup} />
+          </Stack.Navigator>
+          :
+          <Stack.Navigator headerMode='none'>
+            <Stack.Screen name="Main" component={Navigation} />
+          </Stack.Navigator>
+        }
+      </NavigationContainer>
+    </LoginProvider>
+
   );
 }
 
@@ -26,4 +50,27 @@ const styles = StyleSheet.create({
   },
 
 });
+
+
+
+
+// const MainNavigator = () => (
+
+//   <Tab.Navigator>
+//     <Tab.Screen name="Home" component={HomeScreen} />
+//     <Tab.Screen name="Feed" component={FeedScreen} />
+//     <Tab.Screen name="Catalog" component={CatalogScreen} />
+//     <Tab.Screen name="Account" component={AccountScreen} />
+//   </Tab.Navigator>
+
+// )
+
+// const AppNavigator = () => (
+//   <Stack.Navigator headerMode='none'>
+//     <Stack.Screen name="SignIn" component={SignInScreen} />
+//     <Stack.Screen name="SignUp" component={SignUpScreen} />
+//     <Stack.Screen name="Main" component={MainNavigator} />
+//   </Stack.Navigator>
+
+
 
--- pages/Homepage.js
+++ pages/Homepage.js
@@ -1,15 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, {useContext, useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, Button, Form } from 'react-native';
 import Homepagekeytask from '../components/Homepagekeytask';
 import Homepagekeydates from '../components/Homepagekeydates';
 import Homepagerewards from '../components/Homepagerewards';
 import Homepageheader from '../components/Homepageheader';
+import { LoginContext } from '../LoginContext';
 
-export default function Homepage() {
+const Homepage = () => {
+
+
+    const [token, setToken] = useContext(LoginContext)
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setToken("")
+        console.log(token)
+        console.log("token changed")
+    }
+
+
     return (
         <ScrollView contentContainerStyle={styles.layout}>
             <Homepageheader />
+            <Form onSubmit={handleSubmit}>
+            <Button type="submit">Logout</Button>
+            </Form>
             <Homepagekeytask />
             <Homepagekeydates />
             <Homepagerewards />
@@ -17,6 +33,8 @@ export default function Homepage() {
         </ScrollView>
     );
 }
+
+export default Homepage;
 
 
 const styles = StyleSheet.create({
